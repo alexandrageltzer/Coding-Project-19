29803a5dd3d2e4b0799961e779205caebc455c37
alexandrageltzer <geltzer@usf.edu>
2024-07-19 17:53:28 -0400

    setup root component with global state and routing

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Gallery from './Gallery';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tour Comparison</h1>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
